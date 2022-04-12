6c53f1dcfdfae517fe891502667a18fa8963dc0b
Steve Dibb <steve.dibb@gmail.com>
2022-04-12 03:36:49 -0600

    Handle out of boundaries track selection

--- libavformat/dvd.c
+++ libavformat/dvd.c
@@ -128,8 +128,8 @@ static int dvd_open(URLContext *h, const char *path, int flags)
         return AVERROR(EIO);
     }
 
-    /* play first title if none is given */
-    if (dvd->title < 1) {
+    /* play first title if none is given or exceeds boundary */
+    if (dvd->title < 1 || dvd->title > num_title_idx || dvd->title > 99) {
         dvd->title = 1;
     }
 
